bfd68122ebf6b4dba2e062672c6f29b26c471d66
Jhon Jairo Cruz Jiménez <jcruz@terecomiendo.com>
2018-02-11 11:06:53 -0500

    remove skype api

--- js/index.js
+++ js/index.js
@@ -1,33 +1,33 @@
-$(window).load(function(){
-	// Initialize portfolio masonry
-	$('.cards').masonry({
-		itemSelector       : '.card',
-		transitionDuration : 0
-	});
+$(window).load(function () {
+    // Initialize portfolio masonry
+    $('.cards').masonry({
+        itemSelector: '.card',
+        transitionDuration: 0
+    });
 });
 
 (function ($) {
-	'use strict';
-	
-	var $body = $('body'),
-	    $header = $('header'),
-		$hero = $('.hero'),
-		$menuTrigger = $('.menu'),
-		menuHeight = $('.top-bar').height(),
-		$triggers = $('nav [href^="#"]'),
-		$scrollItems = $triggers.map(function () {
-			return $($(this).attr('href'));
-	    }),
-		lastId;
-    
+    'use strict';
+
+    var $body = $('body'),
+        $header = $('header'),
+        $hero = $('.hero'),
+        $menuTrigger = $('.menu'),
+        menuHeight = $('.top-bar').height(),
+        $triggers = $('nav [href^="#"]'),
+        $scrollItems = $triggers.map(function () {
+            return $($(this).attr('href'));
+        }),
+        lastId;
+
 
     $(window)
-        .scroll(function() {
+        .scroll(function () {
             // Handle active menu links
             var pageTop = $(this).scrollTop(),
                 fromTop = pageTop + menuHeight + 50,
                 mql = window.matchMedia("(max-width:900px)"),
-                currentElement = $scrollItems.map(function() {
+                currentElement = $scrollItems.map(function () {
                     if ($(this).offset().top < fromTop) {
                         return this;
                     }
@@ -55,9 +55,22 @@ $(window).load(function(){
                 $hero.toggleClass('sticky', pageTop > 333);
             }
         })
-        .resize(function() {
+        .resize(function () {
             $(this).trigger('scroll');
         })
         .trigger('scroll');
+
+    // Manually trigger menu visibility
+    $menuTrigger.on('click', function () {
+        $body.toggleClass('collapsed');
+    });
+
+    // Navigation
+    $('[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - menuHeight - 44
+        }, 250);
+    });
 
 })(jQuery);
